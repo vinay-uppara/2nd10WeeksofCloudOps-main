36fdc78fc9bdcb022b517c3254bded42312ecba7
vinay-uppara <vinaykumaruppara36@gmail.com>
2025-04-30 00:19:06 +0530

    Update main.tf

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
  #Creating IAM role for EKS
   resource "aws_iam_role" "master" {
-    name = "ec2-admin"
+    name = "veera-eks-worker1"
 
     assume_role_policy = jsonencode({
       "Version": "2012-10-17",
@@ -36,7 +36,7 @@ provider "aws" {
   }
 
   resource "aws_iam_role" "worker" {
-    name = "ec2-admin"
+    name = "veera-eks-worker1"
 
     assume_role_policy = jsonencode({
       "Version": "2012-10-17",
